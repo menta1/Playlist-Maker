67293010ba75fa22255f1728c8bc54931d557ac4
Menta1 <exen1993@gmail.com>
2023-02-03 18:34:52 +0300

    sec com

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search = findViewById<Button>(R.id.search)
+        val mediateka = findViewById<Button>(R.id.mediateka)
+        val settings = findViewById<Button>(R.id.settings)
+
+        search.setOnClickListener{
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        mediateka.setOnClickListener{
+            val mediatekaIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(mediatekaIntent)
+        }
+
+        settings.setOnClickListener{
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
     }
 }
