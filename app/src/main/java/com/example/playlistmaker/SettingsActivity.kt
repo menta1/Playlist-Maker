ba378293c053dcae32d249dbdd9ae63e89575b86
Menta1 <exen1993@gmail.com>
2023-02-12 12:06:25 +0300

    add editText not finish

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,11 +12,33 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<Button>(R.id.backButtonSettingsActivity)
-
+        val shareAppButton = findViewById<Button>(R.id.shareApp)
+        val writeSupportButton = findViewById<Button>(R.id.writeSupport)
+        val termsUseButton  = findViewById<Button>(R.id.termsUse)
         backButton.setOnClickListener{
-            val backButtonIntent = Intent(this, MainActivity::class.java)
-            startActivity(backButtonIntent)
             finish()
         }
+
+        shareAppButton.setOnClickListener{
+            val shareAppButtonIntent = Intent(Intent.ACTION_SEND)
+            shareAppButtonIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/")
+            shareAppButtonIntent.type = "plain/text"
+
+            startActivity(shareAppButtonIntent)
+        }
+        writeSupportButton.setOnClickListener{
+            val writeSupportButtonIntent = Intent(Intent.ACTION_SENDTO)
+            writeSupportButtonIntent.data = Uri.parse("mailto:")
+            writeSupportButtonIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("exen1993@gmail.com"))
+            writeSupportButtonIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            writeSupportButtonIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            startActivity(writeSupportButtonIntent)
+        }
+        termsUseButton.setOnClickListener{
+            val termsUseButtonIntent = Intent(Intent.ACTION_VIEW, Uri.parse(
+                "https://yandex.ru/legal/practicum_offer/"))
+            startActivity(termsUseButtonIntent)
+        }
     }
+
 }
