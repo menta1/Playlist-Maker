9286891f71467125b131de271423a19d24ff4130
Menta1 <Bulatov1993>
2023-02-17 05:07:35 +0500

    Готово

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,72 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    lateinit var inputEditText: EditText
+    companion object{
+        const val TEXT_EDITTEXT = "TEXT_EDITTEXT"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_EDITTEXT, inputEditText.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputEditText.setText(savedInstanceState.getString(TEXT_EDITTEXT))
+    }
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+
+        val backButton = findViewById<ImageView>(R.id.backButton)
+        inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearButton)
+
+        backButton.setOnClickListener{
+            finish()
+        }
+
+        clearButton.setOnClickListener{
+            inputEditText.setText("")
+        }
+
+        var searchTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        }
+
+        inputEditText.addTextChangedListener(searchTextWatcher)
+
     }
+
+    fun clearButtonVisibility(s: CharSequence?) : Int{
+        return if(s.isNullOrEmpty()){
+            View.GONE
+        }else{
+            View.VISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class SettingsActivity : AppCompatActivity() {
+class   SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
