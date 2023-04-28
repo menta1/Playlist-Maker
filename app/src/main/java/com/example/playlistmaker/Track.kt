9a80bb347f42b802221a7279212b19a10962ebaa
Menta1 <exen1993@gmail.com>
2023-04-28 22:18:04 +0300

    Sprint 14

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -14,7 +14,8 @@ data class Track(
     val collectionName: String?,
     val releaseDate: String?,
     val primaryGenreName: String?,
-    val country: String?
+    val country: String?,
+    val previewUrl: String?
 ): Parcelable
 
 
