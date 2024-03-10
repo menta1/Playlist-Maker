28679622ae8f8021547155ee8bc2661d6304e41a
Menta1 <exen1993@gmail.com>
2024-03-10 23:09:21 +0300

    Add Internet check receiver

---
+++ app/src/main/java/com/example/playlistmaker/utils/isConnected.kt
@@ -0,0 +1,24 @@
+package com.example.playlistmaker.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun isConnected(context: Context?): Boolean {
+    val connectivityManager = context?.getSystemService(
+        Context.CONNECTIVITY_SERVICE
+    ) as ConnectivityManager
+    val capabilities =
+        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    if (capabilities != null) {
+        when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+        }
+    }
+    return false
+}
